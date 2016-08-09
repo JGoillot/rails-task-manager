518ddbbbcde4c8090511732dbf37d2abe445b437
JFachetti <goillot.jeremy@gmail.com>
2016-08-09 16:29:10 +0200

    Adding Bootstrap

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,20 +4,38 @@ class MoviesController < ApplicationController
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movies_path
   end
 
   def edit
+    @movie = Movie.find(params[:id])
   end
 
   def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movies_path
   end
 
   def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :genre, :watched,)
   end
 end
